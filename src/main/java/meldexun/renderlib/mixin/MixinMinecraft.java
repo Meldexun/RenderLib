9c26575e9ab42634e092ec3ac21cad4f9bf237f2
Meldexun <27779321+Meldexun@users.noreply.github.com>
2022-05-17 00:58:32 +0200

    Fix render tick events

--- src/main/java/meldexun/renderlib/mixin/MixinMinecraft.java
+++ src/main/java/meldexun/renderlib/mixin/MixinMinecraft.java
@@ -1,15 +1,35 @@
 package meldexun.renderlib.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.util.Timer;
 import net.minecraft.util.math.MathHelper;
 
 @Mixin(Minecraft.class)
 public class MixinMinecraft {
+
+	@Shadow
+	private boolean isGamePaused;
+	@Shadow
+	private float renderPartialTicksPaused;
+	@Shadow
+	private Timer timer;
+
+	@ModifyArg(method = "runGameLoop", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/fml/common/FMLCommonHandler;onRenderTickStart(F)V"))
+	public float onRenderTickStart(float partialTick) {
+		return isGamePaused ? renderPartialTicksPaused : timer.renderPartialTicks;
+	}
+
+	@ModifyArg(method = "runGameLoop", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/fml/common/FMLCommonHandler;onRenderTickEnd(F)V"))
+	public float onRenderTickEnd(float partialTick) {
+		return isGamePaused ? renderPartialTicksPaused : timer.renderPartialTicks;
+	}
 
 	@Inject(method = "getLimitFramerate", cancellable = true, at = @At("HEAD"))
 	public void getLimitFramerate(CallbackInfoReturnable<Integer> info) {
