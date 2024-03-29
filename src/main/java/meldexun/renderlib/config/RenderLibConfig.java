f16ff97a2a0d57a42a4ceb79e2ea8d15934b449e
Meldexun <27779321+Meldexun@users.noreply.github.com>
2024-03-29 13:52:54 +0100

    Remove restart requirement when enabling/disabling opengl debug ouput

--- src/main/java/meldexun/renderlib/config/RenderLibConfig.java
+++ src/main/java/meldexun/renderlib/config/RenderLibConfig.java
@@ -55,7 +55,6 @@ public class RenderLibConfig {
 	public static class OpenGLDebugConfiguration {
 
 		@Config.Comment("Better debugging of OpenGL errors. Might not be supported by your hardware/driver.")
-		@Config.RequiresMcRestart
 		public boolean enabled = false;
 		@Config.Comment("Enable/Disable logging of high severity non-error messages. (You probably never want to enable this as a normal user)")
 		public boolean logHighSeverity = false;
